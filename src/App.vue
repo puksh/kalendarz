ccb2c41133a1fbfea252a8754e515e1d5eadd78b
Lucas <puksh@pm.me>
2024-11-02 22:21:20 +0100

    chore: Init commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="app-container">
+    <Sidebar @navigate="handleNavigation" />
+    <main class="main-content">
+      <fishChecklist v-if="currentPage === 'Calendar'" />
+      <About v-if="currentPage === 'about'" />
+      <Settings v-if="currentPage === 'settings'" />
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueCalendar",
+  components: {
+    Calendar: () => import("./components/Calendar.vue"),
+    SideBar: () => import("./components/SideBar.vue"),
+    About,
+    Settings,
+  },
+  data() {
+    return {
+      currentPage: "fishChecklist",
+    };
+  },
+  methods: {
+    handleNavigation(section) {
+      this.currentPage = section;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: "IBMPlexMono", monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  image-rendering: pixelated;
+  scrollbar-width: none;
+}
+body::-webkit-scrollbar {
+  display: none; /* Hide scrollbar on Chrome, Safari */
+}
+.app-container {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 88px;
+}
+
+.main-content {
+  flex: 1;
+  padding: 20px;
+  max-width: 800px;
+  text-align: center;
+  border-left: 4px solid transparent;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  overflow: visible;
+}
+/* Remove left padding on smaller screens */
+@media (max-width: 900px) {
+  .app-container {
+    padding-left: 0;
+    height: auto;
+  }
+
+  .main-content {
+    padding-bottom: 60px; /* Offset for bottom-positioned sidebar */
+  }
+}
+</style>
