a69cd040474ce729d8fee99d14298176572ceb6c
Lucas <puksh@pm.me>
2024-11-23 20:53:57 +0100

    style: Removed not used styles

--- src/App.vue
+++ src/App.vue
@@ -51,35 +51,14 @@ body::-webkit-scrollbar {
   display: none; /* Hide scrollbar on Chrome, Safari */
 }
 .app-container {
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  align-items: center;
-  padding-left: 88px;
 }
 
 .main-content {
-  display: flex;
   padding: 20px;
   max-width: 1600px;
-  height: auto;
   text-align: center;
-  border-left: 4px solid transparent;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-  overflow-x: visible;
+  overflow-x: visible !important;
   overflow-y: hidden;
-  justify-content: center;
-}
-/* Remove left padding on smaller screens */
-@media (max-width: 900px) {
-  .app-container {
-    padding-left: 0;
-    height: auto;
-  }
-
-  .main-content {
-    padding-bottom: 60px; /* Offset for bottom-positioned sidebar */
-  }
+  padding-bottom: 60px; /* Offset for bottom-positioned sidebar */
 }
 </style>
