6a48a2ac5edbd1077d709691070ad0a3735dc2ef
Lucas <puksh@pm.me>
2024-11-30 17:51:34 +0100

    chore: Eslint config + Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,27 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import pluginPrettier from "eslint-plugin-prettier";
 
 export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
   },
 
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
 
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-]
+  ...pluginVue.configs["flat/essential"],
+  {
+    name: "prettier",
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
