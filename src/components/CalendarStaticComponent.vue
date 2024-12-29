05f9ddfe518286ee59bde926be1cf4d42d33ae16
Lucas <puksh@pm.me>
2024-12-29 18:47:17 +0100

    refactor: move Shift Counts to a component

--- src/components/CalendarStaticComponent.vue
+++ src/components/CalendarStaticComponent.vue
@@ -143,43 +143,17 @@
       </div>
     </div>
   </section>
-  <section class="shift-counts-window">
-    <h3>Ilość zmian</h3>
-    <div style="display: flex; flex-direction: row">
-      <div style="display: flex; flex-direction: column">
-        <h4>Ratowniczki/cy</h4>
-        <ul class="shift-counts">
-          <li
-            v-for="person in people.filter((p) => p.ratownik)"
-            :key="person.id"
-          >
-            {{ person.name }}:
-            <strong>{{ person.shiftCount || 0 }}</strong> zmian/a
-          </li>
-        </ul>
-      </div>
-      <div style="display: flex; flex-direction: column">
-        <h4>Pielęgniarki/rze</h4>
-        <ul class="shift-counts">
-          <li
-            v-for="person in people.filter((p) => !p.ratownik)"
-            v-bind:key="person.id"
-          >
-            {{ person.name }}:
-            <strong>{{ person.shiftCount || 0 }}</strong> zmian/a
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
+  <ShiftCountWindow :people="people" :monthDays="monthDays" />
 </template>
 
 <script>
 import { daysOfWeek } from "@/data/daysOfWeek.js";
 import { addNotification } from "./NotificationMessage.vue";
+import ShiftCountWindow from "./ShiftCountWindow.vue";
 
 export default {
   name: "CalendarComponent",
+  components: { ShiftCountWindow },
   data() {
     return {
       selectedMonth: new Date().getMonth(), // 0-indexed (January = 0)
@@ -714,20 +688,5 @@ export default {
   backdrop-filter: blur(var(--glass-blur)) !important;
   -webkit-backdrop-filter: blur(var(--glass-blur));
   box-shadow: var(--glass-box-shadow);
-}
-.shift-counts {
-  gap: var(--spacing-small);
-  flex-wrap: wrap;
-}
-.shift-counts-window {
-  width: 40% 600px;
-  background: var(--glass-bg-color);
-  backdrop-filter: blur(var(--glass-blur));
-  -webkit-backdrop-filter: blur(var(--glass-blur));
-  border: 1px solid var(--glass-border-color);
-  box-shadow: var(--glass-box-shadow);
-  border-radius: 8px;
-  align-self: center;
-  padding: var(--spacing-medium);
 }
 </style>
