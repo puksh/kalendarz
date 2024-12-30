4c755c266c57b1975cb820ada79e839719aa64c9
Lucas <puksh@pm.me>
2024-12-30 10:01:53 +0100

    feat: PWA support

--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+
 createApp(App).mount("#app");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope,
+        );
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
