ffc77d949110296b7caaf7d0772d8c84e4dd4499
Lucas <85502182+puksh@users.noreply.github.com>
2025-04-10 23:11:22 +0200

    feat: add rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
