0a3f033da58edaa5c993d9e3f19aa76797345268
Lucas <85502182+puksh@users.noreply.github.com>
2025-04-10 23:10:44 +0200

    feat: added vite-compression

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,22 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    visualizer({
+      filename: "stats.html",
+      open: false,
+      gzipSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
