67a6f3fc70f638609d3e86ec8f1e41002c4cd72a
Lucas <85502182+puksh@users.noreply.github.com>
2025-06-10 03:00:00 +0200

    chore(app): move the pencil icon to a component

--- src/App.vue
+++ src/App.vue
@@ -50,41 +50,9 @@
           @change="emitEditingMode($event.target.checked)"
           aria-label="Przełącz tryb edytowania"
         />
-        <span class="slider" role="switch" :aria-checked="isEditingMode">
-          <svg
-            v-if="!isEditingMode"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="3"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="pencil-icon"
-          >
-            <path d="M12 20h9"></path>
-            <path
-              d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
-            ></path>
-          </svg>
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="3"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="pencil-icon"
-          >
-            <path d="M12 20h9"></path>
-            <path
-              d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
-            ></path>
-          </svg>
-        </span> </label
-      ><ButtonExport
+        <PencilIcon :isEditing="isEditingMode" />
+      </label>
+      <ButtonExport
         :people="people"
         :monthDays="monthDays"
         :selectedMonth="selectedMonth"
@@ -173,6 +141,7 @@
 
 <script>
 import { defineAsyncComponent, markRaw } from 'vue';
+import PencilIcon from './components/PencilIcon.vue';
 import { checkShiftDataSync } from '@/utils/dataSync.js';
 import { addNotification } from './components/NotificationMessage.vue';
 import RefreshIcon from './components/icons/RefreshIcon.vue';
@@ -199,7 +168,8 @@ export default {
     ),
     RefreshIcon,
     ButtonExport,
-    ButtonImport
+    ButtonImport,
+    PencilIcon
   },
   data() {
     return {
---
+++ src/components/PencilIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <span class="slider" role="switch" :aria-checked="isEditing">
+    <svg
+      v-if="!isEditing"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="pencil-icon"
+    >
+      <path d="M12 20h9"></path>
+      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+    </svg>
+    <svg
+      v-else
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="pencil-icon"
+    >
+      <path d="M12 20h9"></path>
+      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+    </svg>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'PencilIcon',
+  props: {
+    isEditing: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
